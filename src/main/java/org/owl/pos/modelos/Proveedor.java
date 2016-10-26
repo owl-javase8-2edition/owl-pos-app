05748da8554e93f103d48aa6774630a7ef0f0631
jorgeacaceres <jorge.caceres2894@gmail.com>
2016-10-26 16:51:16 -0300

    agregando proveedor

---
+++ src/main/java/org/owl/pos/modelos/Proveedor.java
@@ -0,0 +1,26 @@
+package org.owl.pos.modelos;
+import java.util.Date;
+public class Proveedor/*extends Persona*/{
+	public Date fechaAlta;
+	public Date fechaBaja;
+	public char estado;
+
+	 public Date getFechaAlta() {
+        return fechaAlta;
+    }
+     public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+     public Date getFechaBaja() {
+        return fechaBaja;
+    }
+     public void setFechaBaja(Date fechaBaja) {
+        this.fechaBaja = fechaBaja;
+    }
+      public char getEstado() {
+        return estado;
+    }
+     public void setEstado(char estado) {
+        this.estado = estado;
+    }
+}
